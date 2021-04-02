2abb8380be527b8b39064dbfa65476cf7ed90ed7
PodayPatron <nazarpikachu18@gmail.com>
2021-04-02 16:45:23 +0300

    ch

---
+++ lesson2-extra/main.js
@@ -0,0 +1,34 @@
+
+
+const accordionItemHeaders = document.getElementsByClassName('accordion-item-header');
+var i;
+
+for (i = 0; i <  accordionItemHeaders.length; i++) {
+  accordionItemHeaders[i].addEventListener('click', function() {
+    this.classList.toggle('active');
+    var panel = this.nextElementSibling;
+    
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
+  });
+}
+
+var accordionItemHeaders2 = document.querySelectorAll('.accordion-item-header2');
+console.log(accordionItemHeaders2);
+
+for(item of accordionItemHeaders2) {
+  item.addEventListener('click', function() {
+
+    if(this.classList.contains('active')) {
+      this.classList.remove('active');
+    } else {
+      for (el of accordionItemHeaders2) {
+        el.classList.remove('active');
+      }
+      this.classList.add('active');
+    }
+  });
+}
