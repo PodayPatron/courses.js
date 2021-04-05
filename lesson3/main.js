b84c2bb0918c9b9ba6ceac04db90a0b67dbe76e6
PodayPatron <nazarpikachu18@gmail.com>
2021-04-05 22:43:19 +0300

    sd

--- lesson3/main.js
+++ lesson3/main.js
@@ -1,1 +1,17 @@
+var $tabsHeader = $('.tabs-header > div');
 
+$tabsHeader.each(function() {
+	$('.js-tab-trigger').on('click', function(e) { 
+		e.preventDefault();
+
+		var $tabsName = $(this).data('tab');
+		var $tab = $('.js-tab-content[data-tab="'+$tabsName+'"]');
+
+		$(this).siblings().removeClass('active');
+		$(this).addClass('active');
+		
+		$('.js-tab-content').removeClass('active');
+		$tab.addClass('active');
+		
+	});
+});
