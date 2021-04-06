89b4c42b3535bff069c48cc0704779016168e37a
PodayPatron <nazarpikachu18@gmail.com>
2021-04-06 10:10:58 +0300

    ch

--- lesson3/main.js
+++ lesson3/main.js
@@ -1,17 +1,33 @@
-var $tabsHeader = $('.tabs-header > div');
+// var $tabsHeader = $('.tabs-header > div');
 
-$tabsHeader.each(function() {
-	$('.js-tab-trigger').on('click', function(e) { 
+// $tabsHeader.each(function() {
+// 	$('.js-tab-trigger').on('click', function(e) { 
+// 		e.preventDefault();
+
+// 		var $tabsName = $(this).data('tab');
+// 		var $tab = $('.js-tab-content[data-tab="'+$tabsName+'"]');
+
+// 		$(this).siblings().removeClass('active');
+// 		$(this).addClass('active');
+		
+// 		$('.js-tab-content').removeClass('active');
+// 		$tab.addClass('active');
+		
+// 	});
+// });
+
+var $tabs = $('.tabs');
+
+$tabs.each(function() {
+	$('.tabs-header > div').on('click', function(e) {
 		e.preventDefault();
 
 		var $tabsName = $(this).data('tab');
-		var $tab = $('.js-tab-content[data-tab="'+$tabsName+'"]');
-
 		$(this).siblings().removeClass('active');
 		$(this).addClass('active');
 		
+		var $tab = $('.js-tab-content[data-tab="'+$tabsName+'"]');
 		$('.js-tab-content').removeClass('active');
 		$tab.addClass('active');
-		
 	});
 });
