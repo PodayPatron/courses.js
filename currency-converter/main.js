ea3a3f3ee352ba4a9cf5aa79ddea4a4108914be1
PodayPatron <nazarpikachu18@gmail.com>
2021-04-12 16:58:15 +0300

    sd

---
+++ currency-converter/main.js
@@ -0,0 +1,25 @@
+(function($) {
+	$(document).ready(function() {
+		var select = $('select');
+		var input = $('input');
+		var key = '2d46c190824f3aa154b6fedb0a718e26';
+		var html = '';
+
+		function getCurrencies() {
+			$.ajax({
+				url: `http://apilayer.net/api/list?access_key=${key}`,
+				success: function (data) {
+					var arrKeys = data.currencies;
+					console.log(arrKeys);
+
+					for(let item in arrKeys) {
+						select.append( `<option value=${item}>${item}</option>`)
+					}
+				}
+			});
+		}
+
+
+		getCurrencies();
+	});
+   })(jQuery);
