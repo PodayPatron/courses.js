e7e7cc67996e92f2e5d4b8235cb23d6bd26aad9f
PodayPatron <nazarpikachu18@gmail.com>
2021-04-12 12:43:08 +0300

    sd

--- weather proj/main.js
+++ weather proj/main.js
@@ -41,52 +41,57 @@
 			});
 		}
 
-		$(document).on('click', '.temperature-value', function() {
-			var fuhrenNum = function celsiusToFahrenheit(temp){
-				return (temp * 9/5) + 32;
-			}
-			var fuhrenResult = fuhrenNum(fahren);
-
-			if($('.temperature-value').find('.celsius')){
-				$('.temperature-value').text(' ').append(
-					`<p class="fahrengeit">${fuhrenResult}°<span>F</span></p>`
-				);
-			}
-		});
-
-		$(document).on('click', '.search-btn', function(e) {
-			e.preventDefault();
-			var valueInput = $('.search-input').val();
-
-			$.ajax({
-				url: `http://api.openweathermap.org/data/2.5/weather?q=${valueInput}&lang=ua&appid=${key}`,
-				success: function (data) {
-					$('.weather-card').remove();
-					$('.wrapper-card').append(
-					`<div class="weather-card">
-						<div class="app-title">
-							<p>Weather</p>
-						</div>
-							<div class="notification"></div>
-						<div class="weather-container">
-							<div class="weather-icon">
-								<img src="./icons/${data.weather[0].icon}.png"/>
-							</div>
-							<div class="temperature-value">
-								<p class="celsius">${Math.floor(data.main.temp - kelvin)}°<span>C</span></p>
-							</div>
-							<div class="temperature-description">
-								<p>${data.weather[0].description}</p>
-							</div>
-							<div class="location">
-								${data.name}, ${data.sys.country}
-							</div>
-						</div>
-					</div>`);
+		function toFuhrengeit() {
+			$(document).on('click', '.temperature-value', function() {
+				var fuhrenNum = function celsiusToFahrenheit(temp){
+					return (temp * 9/5) + 32;
 				}
+				var fuhrenResult = fuhrenNum(fahren);
+	
+				if($(this).find('.celsius')){
+					$(this).text(' ').append(
+						`<p class="fahrengeit">${fuhrenResult}°<span>F</span></p>`
+					);
+				} 
 			});
-		});
+		}
 
-		
+		function btnSearchCity() {
+			$(document).on('click', '.search-btn', function(e) {
+				e.preventDefault();
+				var valueInput = $('.search-input').val();
+	
+				$.ajax({
+					url: `http://api.openweathermap.org/data/2.5/weather?q=${valueInput}&lang=ua&appid=${key}`,
+					success: function (data) {
+						$('.weather-card').remove();
+						$('.wrapper-card').append(
+						`<div class="weather-card">
+							<div class="app-title">
+								<p>Weather</p>
+							</div>
+								<div class="notification"></div>
+							<div class="weather-container">
+								<div class="weather-icon">
+									<img src="./icons/${data.weather[0].icon}.png"/>
+								</div>
+								<div class="temperature-value">
+									<p class="celsius">${Math.floor(data.main.temp - kelvin)}°<span>C</span></p>
+								</div>
+								<div class="temperature-description">
+									<p>${data.weather[0].description}</p>
+								</div>
+								<div class="location">
+									${data.name}, ${data.sys.country}
+								</div>
+							</div>
+						</div>`);
+					}
+				});
+			});
+		}
+
+		toFuhrengeit();
+		btnSearchCity();
 	});
 })(jQuery);
