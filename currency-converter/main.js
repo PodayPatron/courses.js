9f3b02f8784fa7ec457840d1719894a670811456
PodayPatron <nazarpikachu18@gmail.com>
2021-04-14 17:08:22 +0300

    ch

--- currency-converter/main.js
+++ currency-converter/main.js
@@ -1,24 +1,51 @@
 (function($) {
 	$(document).ready(function() {
-		var select = $('select');
-		var input = $('input');
-		var key = '2d46c190824f3aa154b6fedb0a718e26';
+		var $select = $('select');
+		var $selectBase = $('#base');
+		var $selectTarget = $('#target');
+		var $targetNumber = $('#targetNumber');
+		var $baseNumber = $('#baseNumber');
+		var key = '3bc6bce016100d72b8e0315f19c2d33f';
 
 		function getCurrencies() {
 			$.ajax({
 				url: `http://apilayer.net/api/list?access_key=${key}`,
 				success: function (data) {
 					var arrKeys = data.currencies;
-					console.log(arrKeys);
 
 					for(let item in arrKeys) {
-						select.append( `<option value=${item}>${item}</option>`)
+						$select.append( `<option value=${item}>${arrKeys[item]}</option>`)
 					}
 				}
 			});
 		}
 
+		function getRates() {
+			$.ajax({
+				url: `http://api.currencylayer.com/live?access_key=${key}`,
+				success: function (data) {
+					var rates = data.quotes;
+					let time = new Date(data.timestamp * 1000).toLocaleString();
+					$('.time').text(time);
+					$('.usd').append('1$ - ' + `${data.quotes.USDUAH.toLocaleString()}` + ' UAN');
+
+					$(document).on('keyup', $baseNumber, function() {
+						$targetNumber.val($baseNumber.val() * rates['USD' + $selectTarget.val()] / rates['USD' + $selectBase.val()]);
+					});
+					$(document).on('keyup',$targetNumber, function() {
+						$baseNumber.val($targetNumber.val() * rates['USD' + $selectBase.val()] / rates['USD' + $selectTarget.val()]);
+					});
+					$(document).on('change', $selectBase, function() {
+						$targetNumber.val($baseNumber.val() * rates['USD' + $selectTarget.val()] / rates['USD' + $selectBase.val()]);
+					});
+					$(document).on('change', $selectTarget, function() {
+						$baseNumber.val($targetNumber.val() * rates['USD' + $selectBase.val()] / rates['USD' + $selectTarget.val()]);
+					});
+				}
+			});
+		}
 
 		getCurrencies();
+		getRates();
 	});
-   })(jQuery);
+})(jQuery);
