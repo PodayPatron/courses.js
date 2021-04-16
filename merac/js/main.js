bbe2a4229f96a7c0779e0a2b27021fb1ed1f400e
PodayPatron <nazarpikachu18@gmail.com>
2021-04-16 17:22:44 +0300

    ch

--- merac/js/main.js
+++ merac/js/main.js
@@ -1,24 +1,92 @@
-var $sliders = $('.swiper-container');
 
-$sliders.each(function() {
-	
-	const swiper = new Swiper($(this)[0], {
-		loop: true,
-		effect: 'fade',
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
-			renderBullet: function (index, className) {
-				return '<span class="' + className + '">' + (index + 1) + '</span>';
-			},
-		},
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-		scrollbar: {
-			el: '.swiper-scrollbar',
-		},
+function sliderAll() {
+	$('.swiper-container').each(function() {
+		var $dataOption = $(this).data('option');
+		new Swiper(this, renderSlider($dataOption));
 	});
-	console.log($(this));
-});
+
+	function renderSlider(data) {
+		
+		var config = {
+			loop : data.loop,
+			slidesPerView : data.slidesPerView,
+		}
+
+		if(data.spaceBetween) {
+			config.spaceBetween = data.spaceBetween;
+		}
+
+		if(data.pagination) {
+			config.pagination = {
+				el: '.swiper-pagination',
+				clickable: true,
+				renderBullet: function (index, className) {
+					return '<span class="' + className + '">' + (index + 1) + '</span>';
+				},
+			}
+		}
+
+		if(data.navigation) {
+			config.navigation = {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			}
+		}
+
+		if(config.slidesPerView >= 1) {
+			config.breakpoints = {
+				993: {
+					slidesPerView: config.slidesPerView,
+				},
+				150: {
+					slidesPerView: 1,
+				},
+			}
+		}
+		console.log(config);
+		return config;
+	}
+}
+
+
+function toTop() {
+	var $toTop = $('.to-top');
+
+	$(document).scroll(function () {
+		if ($(this).scrollTop() > 300) {
+			$toTop.fadeIn();
+		} else {
+			$toTop.fadeOut();
+		}
+	});
+
+	$toTop.click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 400);
+		return false;
+	});
+}
+
+function tabs() {
+	var $tabs = $('.tabs-menu');
+
+	$tabs.each(function() {
+		$(this).find('li').on('click', function(e) {
+			e.preventDefault();
+
+			var $tabsName = $(this).data('tab');
+			$(this).siblings().removeClass('active');
+			$(this).addClass('active');
+			
+			var $tab = $('.tabs-item[data-tab="'+$tabsName+'"]');
+			$('.tabs-item').removeClass('active');
+			$tab.addClass('active');
+		});
+	});
+}
+
+
+sliderAll();
+toTop();
+tabs();
